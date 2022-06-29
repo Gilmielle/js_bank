db02bc7aa8cb9f41f32382dee7f87646cdfae359
Gilmielle <bizarre_princes@mail.ru>
2022-06-29 16:37:54 +0500

    corrections to webpack config (prod mode)

--- js-advanced-diploma-frontend/webpack.config.js
+++ js-advanced-diploma-frontend/webpack.config.js
@@ -4,73 +4,78 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // eslint-disable-next-line no-undef
-module.exports = (env) => ({
-  entry: {
-    index: './src/index.js',
-    'card-details': './src/card-details.js',
-    atms: './src/atms.js',
-    currency: './src/currency.js',
-  },
-  output: {
-    filename: '[name].js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = (env, argv) => {
+  console.log('WEBPACK', argv.mode);
+  return {
+    entry: {
+      index: './src/index.js',
+      'card-details': './src/card-details.js',
+      atms: './src/atms.js',
+      currency: './src/currency.js',
+    },
+    output: {
+      filename: '[name].js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-      {
-        test: /\.(css|scss)$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          env.production ? MiniCssExtractPlugin.loader : 'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2)$/i,
-        type: 'asset/resource',
-      },
+        {
+          test: /\.(css|scss)$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            argv.mode === 'production'
+              ? MiniCssExtractPlugin.loader
+              : 'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|woff|woff2)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Банк Coin',
+        filename: 'index.html',
+        chunks: ['index'],
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Банк Coin | Просмотр счёта',
+        filename: 'card-details.html',
+        chunks: ['card-details'],
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Банк Coin | Банкоматы',
+        filename: 'atms.html',
+        chunks: ['atms'],
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Банк Coin | Валюта',
+        filename: 'currency.html',
+        chunks: ['currency'],
+      }),
+      new MiniCssExtractPlugin({
+        filename: ({ chunk }) => `${chunk.name.replace('/js/', '/css/')}.css`,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Банк Coin',
-      filename: 'index.html',
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Банк Coin | Просмотр счёта',
-      filename: 'card-details.html',
-      chunks: ['card-details'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Банк Coin | Банкоматы',
-      filename: 'atms.html',
-      chunks: ['atms'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Банк Coin | Валюта',
-      filename: 'currency.html',
-      chunks: ['currency'],
-    }),
-    new MiniCssExtractPlugin({
-      filename: ({ chunk }) => `${chunk.name.replace("/js/", "/css/")}.css`,
-    }),
-  ],
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-  },
-});
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+    },
+  };
+};
